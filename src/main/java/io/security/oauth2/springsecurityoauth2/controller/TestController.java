5798773b3e8a073a8084dbb4755c7029daa927e8
od <od@sweettracker.co.kr>
2023-03-09 18:08:13 +0900

    feat: TestController 추가

--- src/main/java/io/security/oauth2/springsecurityoauth2/controller/TestController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/TestController.java
@@ -1,13 +1,25 @@
 package io.security.oauth2.springsecurityoauth2.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @GetMapping
     public String test(){
+        ClientRegistration keycloak =
+            clientRegistrationRepository.findByRegistrationId("keycloak");
+        log.info(keycloak.getClientId());
         return "success";
     }
+
 }
