fe929e642fa702774bb2fb9c487f62c07e8338dc
od <od@sweettracker.co.kr>
2023-03-14 23:23:41 +0900

    feat: Login API 커스텀 2 (PKCE 인증을 위한 추가 파라미터 등록 : OAuth2ClientConfig)

--- src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
 
 @EnableWebSecurity
 public class OAuth2ClientConfig {
@@ -20,32 +20,22 @@ public class OAuth2ClientConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http.authorizeHttpRequests(request ->request
-            .antMatchers("/loginPage").permitAll()
+            .antMatchers("/home").permitAll()
             .anyRequest().authenticated());
         http.logout()
-            .logoutSuccessHandler(oidcLogoutSuccessHandler())
             .invalidateHttpSession(true)
             .clearAuthentication(true)
-            .deleteCookies("JSESSIONID");
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessUrl("/home");
 
-        // API 커스텀 설정
-        http.oauth2Login(oauth2 -> oauth2
-            // 로그인 페이지
-            .loginPage("/loginPage")
-            // 권한 부여 요청 baseUri (default : /oauth2/authorization)
-            .authorizationEndpoint(authorizationEndpointConfig ->
-                authorizationEndpointConfig.baseUri("/oauth2/v1/authorization"))
-            // 인가 응답 baseUri (application.yml, 인가서버의 정보를 바꿔주어야 오류가 나지 않는다)
-            .redirectionEndpoint(redirectionEndpointConfig ->
-                redirectionEndpointConfig.baseUri("/login/oauth2/code/*"))
-        );
+        http.oauth2Login(authLogin ->
+            authLogin.authorizationEndpoint(authEndpoint ->
+                authEndpoint.authorizationRequestResolver(customOAuth2AuthorizationRequestResolver())));
+        http.oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 
-    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler handler =
-            new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-        handler.setPostLogoutRedirectUri("http://localhost:8081");
-        return handler;
+    private OAuth2AuthorizationRequestResolver customOAuth2AuthorizationRequestResolver() {
+        return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
     }
 }
