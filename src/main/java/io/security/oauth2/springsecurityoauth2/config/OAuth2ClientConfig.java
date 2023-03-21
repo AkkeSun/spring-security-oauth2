f28e076e927c4a49c103346408c9b7739fbe2173
od <od@sweettracker.co.kr>
2023-03-21 15:08:03 +0900

    feat: 기본환경 구성 (OAuth2ClientConfig 수정)

--- src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
@@ -1,41 +1,21 @@
 package io.security.oauth2.springsecurityoauth2.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @EnableWebSecurity
 public class OAuth2ClientConfig {
 
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-
         http.authorizeHttpRequests(request ->request
-            .antMatchers("/home").permitAll()
+            .antMatchers("/", "/oauth2Login").permitAll()
             .anyRequest().authenticated());
-        http.logout()
-            .invalidateHttpSession(true)
-            .clearAuthentication(true)
-            .deleteCookies("JSESSIONID")
-            .logoutSuccessUrl("/home");
-
-        http.oauth2Login(authLogin ->
-            authLogin.authorizationEndpoint(authEndpoint ->
-                authEndpoint.authorizationRequestResolver(customOAuth2AuthorizationRequestResolver())));
-        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Client(Customizer.withDefaults());
         return http.build();
-    }
-
-    private OAuth2AuthorizationRequestResolver customOAuth2AuthorizationRequestResolver() {
-        return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
     }
 }
