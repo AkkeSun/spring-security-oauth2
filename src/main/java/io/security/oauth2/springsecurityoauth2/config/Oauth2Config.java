f0b7906565003374e74bc11aff19aa84111bf916
od <od@sweettracker.co.kr>
2023-03-21 15:07:43 +0900

    feat: 기본환경 구성 (OAuth2AuthorizedClientManager Bean 등록)

--- src/main/java/io/security/oauth2/springsecurityoauth2/config/Oauth2Config.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/config/Oauth2Config.java
@@ -2,36 +2,32 @@ package io.security.oauth2.springsecurityoauth2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 
 @Configuration
 public class Oauth2Config {
-    /*
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(){
-        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-    }
-    */
 
-    private ClientRegistration keycloakClientRegistration(){
-        return ClientRegistration.withRegistrationId("keycloak")
-            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-            .clientId("keycloak-client-id")
-            .clientSecret("6DXU8UzsWxOypCOqbeVjLq0sUaQMculC")
-            .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-            .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
-            .scope("openid", "profile", "email", "address", "phone")
-            .authorizationUri("http://localhost:8080/realms/oauth2")
-            .tokenUri("http://localhost:8080/realms/oauth2/token")
-            .userInfoUri("http://localhost:8080/realms/oauth2/userinfo")
-            .userNameAttributeName(IdTokenClaimNames.SUB)
-            .jwkSetUri("http://localhost:8080/realms/oauth2/certs")
-            .clientName("Keycloak")
-            .build();
+    @Bean
+    public OAuth2AuthorizedClientManager auth2AuthorizedClientManager(
+        ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository){
+
+        DefaultOAuth2AuthorizedClientManager auth2AuthorizedClientManager
+            = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientRepository);
+        OAuth2AuthorizedClientProvider clientProvider =
+            OAuth2AuthorizedClientProviderBuilder.builder()
+                .authorizationCode()
+                .password()
+                .clientCredentials()
+                .refreshToken()
+                .build();
+
+        auth2AuthorizedClientManager.setAuthorizedClientProvider(clientProvider);
+        return auth2AuthorizedClientManager;
     }
+
 }
