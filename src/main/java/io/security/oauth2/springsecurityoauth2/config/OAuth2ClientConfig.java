955cae2fc2d463ea5cd844b1939f33e7dcdddc6d
od <od@sweettracker.co.kr>
2023-04-03 17:21:07 +0900

    feat: 소셜 로그인 (기본 설정)

--- src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,17 +14,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class OAuth2ClientConfig {
 
+    // 정적 파일 처리
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web -> web.ignoring().antMatchers(
+            "/static/js/**", "/static/css/**", "static/scss/**"
+        ));
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http.authorizeHttpRequests(request ->request
-            .antMatchers("/home").permitAll()
+            .antMatchers("/").permitAll()
             .anyRequest().authenticated());
         http.logout()
-            .invalidateHttpSession(true)
-            .clearAuthentication(true)
-            .deleteCookies("JSESSIONID")
-            .logoutSuccessUrl("/home");
+            .logoutSuccessUrl("/");
         http.oauth2Login(Customizer.withDefaults());
         return http.build();
     }
