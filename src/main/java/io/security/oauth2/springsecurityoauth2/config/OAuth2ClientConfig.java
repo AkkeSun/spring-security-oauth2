7d3a54df9101257bf91a91f68e721ed9648f3ae3
od <od@sweettracker.co.kr>
2023-04-04 00:30:15 +0900

    feat: Google, Naver 소셜 로그인 구축 (config)

--- src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/config/OAuth2ClientConfig.java
@@ -1,18 +1,21 @@
 package io.security.oauth2.springsecurityoauth2.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.security.oauth2.springsecurityoauth2.service.CustomOAuth2UserService;
+import io.security.oauth2.springsecurityoauth2.service.CustomOidcUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class OAuth2ClientConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOidcUserService customOidcUserService;
 
     // 정적 파일 처리
     @Bean
@@ -24,13 +27,16 @@ public class OAuth2ClientConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-
-        http.authorizeHttpRequests(request ->request
+        http.authorizeRequests(request ->request
+            .antMatchers("/api/user").access("hasAnyRole('SCOPE_profile','SCOPE_email')")
+            .antMatchers("/api/oidc").access("hasAnyRole('SCOPE_openid')")
             .antMatchers("/").permitAll()
             .anyRequest().authenticated());
         http.logout()
             .logoutSuccessUrl("/");
-        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                .userService(customOAuth2UserService)
+                .oidcUserService(customOidcUserService)));
         return http.build();
     }
 
